cdb030b741db3f84f84177c2cec76057f1fc3e8b
samuel922 <onyangosamuel583@gmail.com>
2023-02-22 20:25:30 +0300

    Updated expenses to display dynamically

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import "./App.css"
 import NewExpense from "./components/NewExpense/NewExpense"
 import Expenses from "./components/Expenses/Expenses"
 
-const App = ()  => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: "Toilet Paper",
-      amount: 10.24,
-      date: new Date(2023, 2, 20)
-    },
-    {
-      id: 'e2',
-      title: "Sun Glasses",
-      amount: 50.20,
-      date: new Date(2023, 1, 20)
-    },
-    {
-      id: 'e3',
-      title: "Jordan Sneakers",
-      amount: 70.00,
-      date: new Date(2023, 2, 20)
-    },
-    {
-      id: 'e4',
-      title: "Kaki Trouser",
-      amount: 10.24,
-      date: new Date(2023, 2, 20)
-    },
-  ]
+const INITIAL_EXPENSES = [
+  {
+    id: 'e1',
+    title: "Toilet Paper",
+    amount: 10.24,
+    date: new Date(2023, 2, 20)
+  },
+  {
+    id: 'e2',
+    title: "Sun Glasses",
+    amount: 50.20,
+    date: new Date(2023, 1, 20)
+  },
+  {
+    id: 'e3',
+    title: "Jordan Sneakers",
+    amount: 70.00,
+    date: new Date(2023, 2, 20)
+  },
+  {
+    id: 'e4',
+    title: "Kaki Trouser",
+    amount: 10.24,
+    date: new Date(2023, 2, 20)
+  },
+]
 
+
+const App = ()  => {
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES)
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
   }
   return (
     <div>
